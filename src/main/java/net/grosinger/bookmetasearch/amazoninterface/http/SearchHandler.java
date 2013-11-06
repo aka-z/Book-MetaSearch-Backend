03ef986b55e50c02a1766b76c751fa840d647355
Tony Grosinger <tony@grosinger.net>
2013-11-05 20:03:16 -0800

    Now returing full response converted to JSON

--- src/main/java/net/grosinger/bookmetasearch/amazoninterface/http/SearchHandler.java
+++ src/main/java/net/grosinger/bookmetasearch/amazoninterface/http/SearchHandler.java
@@ -1,6 +1,7 @@
 package net.grosinger.bookmetasearch.amazoninterface.http;
 
 import com.ECS.client.jax.Item;
+import com.google.gson.Gson;
 import net.grosinger.bookmetasearch.amazoninterface.AmazonAffiliatesSearch;
 import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Request;
@@ -20,9 +21,11 @@ import java.util.Map;
 public class SearchHandler extends AbstractHandler {
     private static Logger LOG = Logger.getLogger(SearchHandler.class);
     private AmazonAffiliatesSearch searcher;
+    private Gson gson;
 
     public SearchHandler(AmazonAffiliatesSearch searcher) {
         this.searcher = searcher;
+        gson = new Gson();
     }
 
     @Override
@@ -32,41 +35,38 @@ public class SearchHandler extends AbstractHandler {
 
         response.setContentType("text/html;charset=utf-8");
 
+        // Check the path
         String[] requestPath = request.getPathInfo().substring(1).split("/");
-        if (requestPath.length == 2 && requestPath[0].equals("search")) {
-            LOG.debug("Performing search: " + request.getQueryString());
-
-            if (request.getQueryString() != null) {
-                Map<String, String> parameters = getQueryMap(request);
-
-                if (requestPath[1].equals("asin") && parameters.containsKey("asin")) {
-                        List<Item> results = searcher.performItemLookupASIN(parameters.get("asin"));
-
-                        response.setStatus(HttpServletResponse.SC_OK);
-                        response.getOutputStream().print(results.get(0).getItemAttributes().getTitle());
-                        baseRequest.setHandled(true);
-                } else if(requestPath[1].equals("isbn") && parameters.containsKey("isbn")) {
-                        List<Item> results = searcher.performItemLookupISBN(parameters.get("isbn"));
-
-                        response.setStatus(HttpServletResponse.SC_OK);
-                        response.getOutputStream().print(results.get(0).getItemAttributes().getTitle());
-                        baseRequest.setHandled(true);
-                        return;
-                }
-            }
-
-            // If we haven't returned yet...
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid search request");
+        if (requestPath.length != 1 || !requestPath[0].equals("search") || request.getQueryString() == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
             baseRequest.setHandled(true);
             return;
         }
 
-        LOG.error("Unable to handle request");
-        response.sendError(HttpServletResponse.SC_NOT_FOUND);
-        baseRequest.setHandled(false);
+        // Check the parameters
+        Map<String, String> parameters = getQueryMap(request);
+        if (!parameters.containsKey("method")
+                || !parameters.containsKey("q")
+                || (!parameters.get("method").equals("asin") && !parameters.get("method").equals("isbn"))) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid parameters or search method");
+            baseRequest.setHandled(true);
+            return;
+        }
+
+        // Perform search
+        List<Item> results;
+        if (parameters.get("method").equals("asin")) {
+            results = searcher.performItemLookupASIN(parameters.get("q"));
+        } else {
+            results = searcher.performItemLookupISBN(parameters.get("q"));
+        }
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getOutputStream().print(gson.toJson(results));
+        baseRequest.setHandled(true);
     }
 
-    public static Map<String, String> getQueryMap(HttpServletRequest request) {
+    private Map<String, String> getQueryMap(HttpServletRequest request) {
         String[] params = request.getQueryString().split("&");
         Map<String, String> parameters = new HashMap<String, String>();
         for (String param : params) {
